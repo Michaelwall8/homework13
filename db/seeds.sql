d9348948ae0956300cb9036ff458dda3388965ec
Michael Wall <mikesterwallace8@gmail.com>
2021-02-26 21:32:10 -0500

    Schema and seeds SQL database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (`name`, `devour`) VALUES ('Double Cheese Burger', true);
+INSERT INTO burgers (`name`, `devour`) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (`name`, `devour`) VALUES ('Bacon and Cheese Burger', true);
